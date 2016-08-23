167618bc3e282d65780e9a26de73fdbe6569ad1b
Castulo J. Martinez <castulo.martinez@intel.com>
2016-08-23 09:45:18 -0700

    Added the rest of the functions

---
+++ jenkins/common.groovy
@@ -0,0 +1,23 @@
+#!/usr/bin/env groovy
+
+echo "Loading external common functions..."
+
+
+def wait_for_ping(host_ip) {
+
+    echo "Waiting for the host with IP:${host_ip} to become online."
+    def response
+    def current_time
+    int timeout = 360
+    def initial_time = sh returnStdout: true, script: 'date +%s'
+    waitUntil {
+        current_time = sh returnStdout: true, script: 'date +%s'
+        if (current_time.toInteger() - initial_time.toInteger() > timeout) {
+            error "The host did not respond to ping within the timeout of ${timeout} seconds"
+        }
+        response = sh returnStatus: true, script: "ping -q -c 1 ${host_ip}"
+        return (response == 0)
+    }
+
+}
+
