c6c5b6119de411d10391d347a2312a16f959c5b4
Castulo J. Martinez <castulo.martinez@intel.com>
2016-08-23 10:38:43 -0700

    Fixed the issues with the imports
    Removed the import from the osa_onmetal helper file and added the
    ping function directly into the helper file.
    Also renamed the Jenkinsfile to match the standard.

--- jenkins/osa_onmetal.functions.groovy
+++ jenkins/osa_onmetal.functions.groovy
@@ -1,8 +1,26 @@
 #!/usr/bin/env groovy
-import static common.groovy.*
 
 // Any command placed at the root of the file get executed during the load operation
 echo "Loading external functions for an onMetal host..."
+
+
+def wait_for_ping(host_ip) {
+
+    echo "Waiting for the host with IP:${host_ip} to become online."
+    def response
+    def current_time
+    int timeout = 360
+    def initial_time = sh returnStdout: true, script: 'date +%s'
+    waitUntil {
+        current_time = sh returnStdout: true, script: 'date +%s'
+        if (current_time.toInteger() - initial_time.toInteger() > timeout) {
+            error "The host did not respond to ping within the timeout of ${timeout} seconds"
+        }
+        response = sh returnStatus: true, script: "ping -q -c 1 ${host_ip}"
+        return (response == 0)
+    }
+
+}
 
 
 def onmetal_provision(playbooks_path) {
